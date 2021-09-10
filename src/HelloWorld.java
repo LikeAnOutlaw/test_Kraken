334d025a32128866b546f3b61552c177a1136db8
Konstantinos Rizos <konriz.sd@gmail.com>
2021-09-10 19:07:53 +0300

    Minor changes to the HelloWorld.java file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
 
 	public static void main(String[] args{
-		System.out.println("Hello World, my name is developer 1");
+		System.out.println("Hello World, my name is developer_1");
 	}
 }
